3ef65eb894145def68330f83dced8145ca92e2da
Alex Evers <alex.evers2+github@gmail.com>
2020-12-07 15:36:59 -0800

    feature: added placeholder for profileEdit action

---
+++ src/lib/profileEdit.js
@@ -0,0 +1,47 @@
+
+/*
+
+ProfileEdit control
+
+
+*/
+
+/*
+<Component use:action={parameters}/>
+action = (node: HTMLEelement, parameters: any) => {
+  update?: (parameters: any) => void,
+  destroy?: () => void
+}
+*/
+
+import { createEventDispatcher } from 'svelte';
+import { onMount, setContext, getContext, hasContext } from 'svelte';
+import { writable, readable, derived, get } from "svelte/store";
+
+
+export function profileEdit(node, params) {
+
+  const dispatch = createEventDispatcher();
+  const dashboardWritable = getContext("dashboard");
+
+  const openProfile = (e) => {
+    e.preventDefault();
+    console.log("got command submit", e);
+    // dispatch('openFile', {
+    //   source: "fileset",
+    //   data: file
+    // });
+
+  }
+  node.addEventListener('click', openProfile);
+
+  return {
+    update(newFile) {
+      console.log("profileEdit got update", newFile);
+    },
+    destroy() {
+      console.log("profileEdit is destroyed");
+      node.removeListener('click', openProfile);
+    }
+  };
+}
